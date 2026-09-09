870fceda1c48551a918cd0eea9c44d8217409819
PTOM76 <ptms76@outlook.jp>
2026-09-09 22:20:34 +0900

    feat: add comparison page and update documentation

--- page/compare.php
+++ page/compare.php
@@ -71,10 +71,10 @@ if (Auth::isLoggedIn()) {
                         ? $favorite['thumbnail']
                         : 'https://i.ytimg.com/vi/' . $favorite['video_id'] . '/mqdefault.jpg';
                     ?>
-                    <button type="button" class="compare-picker-item" onclick="CompareVideos.add('<?php echo htmlspecialchars($favorite['video_id'], ENT_QUOTES); ?>')">
+                    <a class="compare-picker-item" onclick="CompareVideos.add('<?php echo htmlspecialchars($favorite['video_id'], ENT_QUOTES); ?>')">
                         <img src="<?php echo htmlspecialchars($thumbnail); ?>" alt=""  width="320px" height="180px" style="width:320px;height:180px;object-fit:cover;" loading="lazy">
                         <span class="compare-picker-title"><?php echo htmlspecialchars($favorite['title']); ?></span>
-                    </button>
+                    </a>
                 <?php endforeach; ?>
             </div>
         <?php endif; ?>
